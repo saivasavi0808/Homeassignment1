90987e0c6f82a887567b7b20b73ec83604b10266
saivasavi0808 <saivasavi62@gmail.com>
2025-12-06 21:11:41 +0530

    Add files via upload

---
+++ Employee/EmployeeDetails.java
@@ -0,0 +1,29 @@
+package Employee;
+
+public class EmployeeDetails {
+
+	    public static void main(String[] args) {
+
+	        // Declaring and initializing variables
+	        String name = "John Doe";
+	        int age = 30;
+	        float salary = 55000.75f;
+	        char gender = 'M';
+	        boolean acceptTerms = true;
+	        float rating = 4.5f;
+	        long mobileNumber = 9876543210L;
+	        int pinCode = 56001;
+	        int luckyNumber = 7;
+	        System.out.println(name);
+	        System.out.println(age);
+	        System.out.println(salary);
+	        System.out.println(gender);
+	        System.out.println(acceptTerms);
+	        System.out.println(rating);
+	        System.out.println(mobileNumber);
+	        System.out.println(pinCode);
+	        System.out.println(luckyNumber);
+	    }
+	}
+
+
